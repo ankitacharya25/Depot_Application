9c5b08208d43b2a04cca4babcef726f9dc2a5083
ankitacharya25 <ankitacharya95@gmail.com>
2021-07-12 04:21:56 +0530

    Task B

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -25,11 +25,25 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @product }
+        format.html { 
+          redirect_to @product, 
+          notice: "Product was successfully created." 
+        }
+        format.json { 
+          render :show, 
+          status: :created, 
+          location: @product 
+        }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        puts @product.errors.full_messages
+        format.html { 
+          render :new, 
+          status: :unprocessable_entity 
+        }
+        format.json { 
+          render json: @product.errors, 
+          status: :unprocessable_entity 
+        }
       end
     end
   end
@@ -38,11 +52,23 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @product }
+        format.html { 
+          redirect_to @product, 
+          notice: "Product was successfully updated." 
+        }
+        format.json { 
+          render :show, status: :ok, 
+          location: @product 
+        }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.html { 
+          render :edit, 
+          status: :unprocessable_entity 
+        }
+        format.json { 
+          render json: @product.errors, 
+          status: :unprocessable_entity 
+        }
       end
     end
   end

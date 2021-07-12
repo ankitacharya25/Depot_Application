4329805588ce1c2a13b147ce7b03cd6dc37a8650
ankitacharya25 <ankitacharya95@gmail.com>
2021-07-12 15:16:50 +0530

    Adding Ajax to Application

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 
   # GET /products/1 or /products/1.json
@@ -60,6 +60,9 @@ class ProductsController < ApplicationController
           render :show, status: :ok, 
           location: @product 
         }
+        @products = Product.all.order(:title)
+        ActionCable.server.broadcast 'products',
+          html: render_to_string('store/index', layout: false)
       else
         format.html { 
           render :edit, 

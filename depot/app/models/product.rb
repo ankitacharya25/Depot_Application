dbec42d6dfef239da2c5c2a8c806a98f38493dd3
ankitacharya25 <ankitacharya95@gmail.com>
2021-07-12 16:10:04 +0530

    Capturing order details & Checkout functionality

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -6,7 +6,6 @@ class Product < ApplicationRecord
   #...
 
   validates :title, :description, :image_url, presence: true
-  #
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
